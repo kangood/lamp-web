76d49b08c7a5c25b030cec8a0af054af084b5a2a
kangod <kangod.yan@gmail.com>
2023-09-28 20:32:43 +0800

    Init: 把3R的Next.JS模板代码的抽取部分单独提出来，做成通用模板，初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -136,11 +136,12 @@ module.exports = {
         'react/react-in-jsx-scope': 0,
         'react/require-default-props': 0,
         'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
-        'react/function-component-definition': [
-            2,
-            { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
-        ],
+        // 'react/function-component-definition': [
+        //     2,
+        //     { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+        // ],
         'react-hooks/exhaustive-deps': 0,
+        'react/function-component-definition': 0,
         /* ********************************** jax-a11y ********************************** */
         'jsx-a11y/anchor-is-valid': 0,
         'jsx-a11y/no-static-element-interactions': 0,
