dab2e65335c596f429277f719735d4439b77cd7c
kangod <kangod.yan@gmail.com>
2023-10-10 15:31:47 +0800

    Fix: 1、去掉@next/font依赖包的引入，从next中获取字体

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { Montserrat } from '@next/font/google';
+import { Montserrat } from 'next/font/google';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
